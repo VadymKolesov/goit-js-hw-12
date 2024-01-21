4cb5a71b8a68953d30014de7232d1dfa1ca21e32
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2024-01-21 21:41:31 +0100

    try

--- src/main.js
+++ src/main.js
@@ -2,80 +2,143 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-let gallery = new SimpleLightbox('.small-image-item a');
+import axios from 'axios';
 
 const refs = {
   searchFormElement: document.querySelector('.search-form'),
   searchInput: document.querySelector('.search-input'),
   searchBtn: document.querySelector('.search-btn'),
   imagesMarkUp: document.querySelector('.images-markup'),
+  loadBtn: document.querySelector('.load-btn'),
 };
 
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '41860696-2902f93b7313ed5ba840414ce';
+const API_KEY = '41860696-2902f93b7313ed5ba840414ce';
+
+const PER_PAGE = 40;
+
+let gallery = new SimpleLightbox('.small-image-item a');
 
 let query = '';
 
+let numOfPage;
+
+const instance = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 refs.searchFormElement.addEventListener('submit', searchQuery);
+refs.loadBtn.addEventListener('click', onLoadMore);
 
 function searchQuery(e) {
   e.preventDefault();
+  numOfPage = 1;
+  refs.imagesMarkUp.innerHTML = ``;
   query = refs.searchInput.value.toLowerCase();
-  refs.imagesMarkUp.innerHTML = `<li><span class="loader"></span></li>`;
+  refs.imagesMarkUp.insertAdjacentHTML(
+    'beforeend',
+    `<li class="loader-item"><span class="loader"></span></li>`
+  );
   fetchImages();
 }
 
-function fetchImages() {
-  fetch(
-    `${BASE_URL}?key=${KEY}&q=${query}&image-type=photo&orientation=horizontal&safesearch=true&per_page=21`
-  )
-    .then(r => {
-      return r.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        refs.imagesMarkUp.innerHTML = ``;
-        iziToast.error({
-          theme: 'dark',
-          position: 'topRight',
-          maxWidth: '392px',
-          messageSize: '16px',
-          messageLineHeight: '24px',
-          backgroundColor: '#EF4040',
-          messageColor: '#FAFAFB',
-          message:
-            'Sorry, there are no images matching your search query. Please, try again!',
-          progressBarColor: '#B51B1B',
-        });
-        return;
-      }
-      const markUp = data.hits.reduce(
-        (html, image) =>
-          html +
-          `<li class="small-image-item"><a href="${image.largeImageURL}"><img src="${image.webformatURL}" class="small-image" alt="${image.tags}" /></a><ul class="description-image-list"><li class="description-image-item"><p class="description-title">Likes</p><p class="description-value">${image.likes}</p></li>
+function onBadQuery() {
+  refs.imagesMarkUp.innerHTML = ``;
+
+  iziToast.error({
+    theme: 'dark',
+    position: 'topRight',
+    maxWidth: '392px',
+    messageSize: '16px',
+    messageLineHeight: '24px',
+    backgroundColor: '#EF4040',
+    messageColor: '#FAFAFB',
+    message:
+      'Sorry, there are no images matching your search query. Please, try again!',
+    progressBarColor: '#B51B1B',
+  });
+}
+
+function onRequestError(error) {
+  refs.imagesMarkUp.innerHTML = ``;
+  refs.loadBtn.classList.add('hidden');
+  iziToast.error({
+    theme: 'dark',
+    position: 'topRight',
+    maxWidth: '392px',
+    messageSize: '16px',
+    messageLineHeight: '24px',
+    backgroundColor: '#EF4040',
+    messageColor: '#FAFAFB',
+    message: `Something went wrong:: ${error}`,
+    progressBarColor: '#B51B1B',
+  });
+}
+
+function checkLimit(limit) {
+  if (refs.imagesMarkUp.childNodes.length >= limit) {
+    refs.loadBtn.classList.add('hidden');
+    refs.imagesMarkUp.insertAdjacentHTML(
+      'beforeend',
+      `<p class="limit-message">"We're sorry, but you've reached the end of search results.</p>`
+    );
+    return;
+  }
+}
+
+function renderMarkUp(data) {
+  document.querySelector('.loader-item').remove();
+  const markUp = data.hits.reduce(
+    (html, image) =>
+      html +
+      `<li class="small-image-item"><a href="${image.largeImageURL}"><img src="${image.webformatURL}" class="small-image" alt="${image.tags}" /></a><ul class="description-image-list"><li class="description-image-item"><p class="description-title">Likes</p><p class="description-value">${image.likes}</p></li>
           <li class="description-image-item"><p class="description-title">Views</p><p class="description-value">${image.views}</p></li>
           <li class="description-image-item"><p class="description-title">Comments</p><p class="description-value">${image.comments}</p></li>
           <li class="description-image-item"><p class="description-title">Downloads</p><p class="description-value">${image.downloads}</p></li></ul></li>`,
-        ''
-      );
-      refs.imagesMarkUp.innerHTML = markUp;
-    })
-    .catch(error => {
-      refs.imagesMarkUp.innerHTML = ``;
-      iziToast.error({
-        theme: 'dark',
-        position: 'topRight',
-        maxWidth: '392px',
-        messageSize: '16px',
-        messageLineHeight: '24px',
-        backgroundColor: '#EF4040',
-        messageColor: '#FAFAFB',
-        message: `Something went wrong:: ${error}`,
-        progressBarColor: '#B51B1B',
-      });
-    })
-    .finally(e => {
-      gallery.refresh();
-    });
+    ''
+  );
+  refs.imagesMarkUp.insertAdjacentHTML('beforeend', markUp);
+
+  checkLimit(data.totalHits);
+
+  gallery.refresh();
+}
+
+async function onLoadMore() {
+  numOfPage += 1;
+  refs.imagesMarkUp.insertAdjacentHTML(
+    'beforeend',
+    `<li class="loader-item"><span class="loader"></span></li>`
+  );
+
+  await fetchImages();
+
+  window.scrollBy({
+    top:
+      document.querySelector('.small-image-item').getBoundingClientRect()
+        .height * 2,
+    left: 0,
+    behavior: 'smooth',
+  });
+}
+
+async function fetchImages() {
+  refs.loadBtn.classList.add('hidden');
+  try {
+    const response = await instance.get(
+      `?key=${API_KEY}&q=${query}&image-type=photo&orientation=horizontal&safesearch=true&page=${numOfPage}&per_page=${PER_PAGE}`
+    );
+    const responseData = await response.data;
+
+    if (responseData.hits.length <= 0) {
+      onBadQuery();
+      return;
+    }
+    refs.loadBtn.classList.remove('hidden');
+    renderMarkUp(responseData);
+  } catch (e) {
+    onRequestError(e.message);
+  }
 }
